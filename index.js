a056639da9fc59aeee172d6f2347e25fe5774c40
Alex Salgado <alexasa.wdc@gmail.com>
2024-07-26 21:52:43 -0600

    feature: instalacion de nodejs, dotenv y xpress

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/hello', (req, res) => {
+    res.send({ message: 'Hello from the server!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
