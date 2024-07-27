140e265539f654b3fca290d1a5550eb3bf107036
Alex Salgado <alexasa.wdc@gmail.com>
2024-07-27 11:55:02 -0600

    feature: se integra nodemailer

--- routes/taxPayers.js
+++ routes/taxPayers.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../config/mysql');
+const sendmail = require('../utils/sendmail');
 
-const checkEmailExists = (email) => {
+const checkEmailExists = async (email) => {
     return new Promise((resolve, reject) => {
         const query = 'SELECT id FROM tax_payers WHERE email = ?';
         connection.query(query, [email], (error, results) => {
@@ -13,7 +14,7 @@ const checkEmailExists = (email) => {
     });
 };
 
-const checkTaxRegimeExists = (taxRegimeId) => {
+const checkTaxRegimeExists = async (taxRegimeId) => {
     return new Promise((resolve, reject) => {
         const query = 'SELECT id FROM tax_regimens WHERE id = ?';
         connection.query(query, [taxRegimeId], (error, results) => {
@@ -24,47 +25,47 @@ const checkTaxRegimeExists = (taxRegimeId) => {
 };
 
 const setSuscribe = async (params) => {
-    return new Promise(async (resolve, reject) => {
-        if (params.email === undefined || params.taxRegimeId === undefined) {
-            return reject('Email and taxRegimeId are required');
+    if (params.email === undefined || params.taxRegimeId === undefined) {
+        throw new Error('Email and taxRegimeId are required');
+    }
+    try {
+        // Verificar si el régimen fiscal existe
+        const taxRegimeExists = await checkTaxRegimeExists(params.taxRegimeId);
+        if (!taxRegimeExists) {
+            throw new Error('The tax regime does not exist');
         }
-        try {
-            // Verificar si el régimen fiscal existe
-            const taxRegimeExists = await checkTaxRegimeExists(params.taxRegimeId);
-            if (!taxRegimeExists) {
-                return reject('The tax regime does not exist');
-            }
 
-            // Verificar si el correo electrónico ya está registrado
-            const emailExists = await checkEmailExists(params.email);
-            if (emailExists) {
-                return reject('The email is already registered');
-            }
+        // Verificar si el correo electrónico ya está registrado
+        const emailExists = await checkEmailExists(params.email);
+        if (emailExists) {
+            throw new Error('The email is already registered');
+        }
 
-            //Si es valido, proceder con la inserción
+        // Si es válido, proceder con la inserción
+        return new Promise((resolve, reject) => {
             const query = 'INSERT INTO tax_payers (email, tax_regime_id) VALUES (?, ?)';
             const values = [params.email, params.taxRegimeId];
             connection.query(query, values, (error, results) => {
                 if (error) return reject(error);
+                sendEmail(params.email, 'Registro SAT', { email: params.email });
                 resolve(results);
             });
-        } catch (error) {
-            reject(error);
-        }
-    });
+        });
+    } catch (error) {
+        throw error;
+    }
 };
 
 const unSuscribe = (params) => {
     return new Promise((resolve, reject) => {
-        if( params.id === undefined ){
-            reject('Id are required');
+        if (params.id === undefined) {
+            reject('Id is required');
         }
 
         const query = 'UPDATE tax_payers SET status = "suspendido" WHERE id = ?';
-        const id = params.id;
-        const values = [id];
+        const values = [params.id];
         connection.query(query, values, (error, results) => {
-            if (error) throw error;
+            if (error) return reject(error);
             resolve(results);
         });
     });
@@ -75,7 +76,7 @@ router.post('/', async (req, res) => {
         const taxPayers = await setSuscribe(req.body);
         res.json(taxPayers);
     } catch (error) {
-        res.status(500).send('Error fetching events');
+        res.status(500).send(error.message);
     }
 });
 
@@ -84,7 +85,7 @@ router.put('/', async (req, res) => {
         const taxPayers = await unSuscribe(req.body);
         res.json(taxPayers);
     } catch (error) {
-        res.status(500).send('Error fetching events');
+        res.status(500).send(error.message);
     }
 });
 
